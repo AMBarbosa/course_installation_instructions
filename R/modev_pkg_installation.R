37762cd99f9f10d3434562ab2529cad14d65f0f0
AMBarbosa <ana.marcia.barbosa@gmail.com>
2023-11-26 20:36:35 +0000

    upload modev pkg install script

---
+++ R/modev_pkg_installation.R
@@ -0,0 +1,19 @@
+# install recent versions of R (and RStudio), and then run the commands below to install the required packages
+
+# EVERY TIME you get a message complaining about a package, install that package and try again
+
+# after installing all packages, load each one with library() and check if there are additional error messages, in which case do as above
+
+# day 1
+install.packages("terra")
+install.packages("fuzzySim")
+install.packages("maxnet")
+install.packages("glmnet")  # required by maxnet
+install.packages("Matrix")  # required by maxnet
+install.packages("modEvA")
+install.packages("pROC")
+
+# day 2
+install.packages("cowplot")  # required by blockCV
+install.packages("automap")  # required by blockCV
+install.packages("blockCV")
