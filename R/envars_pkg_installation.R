7c4201d735d5966456f76c69508c6b3a124d0c6b
AMBarbosa <ana.marcia.barbosa@gmail.com>
2023-12-12 10:53:32 +0000

    upload envars installation instructions

---
+++ R/envars_pkg_installation.R
@@ -0,0 +1,17 @@
+# check RStudio menu option "Code - Soft Wrap Long Lines" and READ THE INSTRUCTIONS below!
+
+# install recent versions of R and RStudio, open RStudio, then run the commands below, one at a time, to install the required packages
+
+# it's preferable (often necessary) to have recent versions of all packages, so install even those you already had
+
+# if you get an error message about Rtools, follow the instructions in that message -- it may involve installing the Rtools software (outside R) and adding it to the path following the instructions in the link provided in the error message
+
+# EVERY TIME you get a message complaining about a package, install that package and try again
+
+# after installing all packages, load each one with library() and check if there are additional error messages, in which case do as above
+
+install.packages("terra")
+install.packages("geodata")
+install.packages("sdmpredictors")
+install.packages("rnaturalearth")
+install.packages("gstat")
