9b7c1cad6b6d4dc6f245b07084536ce3de287161
AMBarbosa <ana.marcia.barbosa@gmail.com>
2024-01-08 11:02:58 +0000

    minor edits to script comments

--- R/envars_pkg_installation.R
+++ R/envars_pkg_installation.R
@@ -1,12 +1,14 @@
-# check RStudio menu option "Code - Soft Wrap Long Lines" and READ THE INSTRUCTIONS below!
+# check RStudio menu option "Code - Soft Wrap Long Lines"
+
+# READ THE INSTRUCTIONS below!
 
 # install recent versions of R and RStudio, open RStudio, then run the commands below, one at a time, to install the required packages
 
-# it's preferable (often necessary) to have recent versions of all packages, so install even those you already had
+# it's preferable (often necessary) to have recent versions of all packages, so install even those you already had!
 
 # if you get an error message about Rtools, follow the instructions in that message -- it may involve installing the Rtools software (outside R) and adding it to the path following the instructions in the link provided in the error message
 
-# EVERY TIME you get a message complaining about a package, install that package and try again
+# EVERY TIME you get an error message complaining about a package, install that package and try again
 
 # after installing all packages, load each one with library() and check if there are additional error messages, in which case do as above
 
